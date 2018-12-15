6b6bae7aa5ee986f0fbb954c705fdb18a78774ca
uda5150 <tks.udagawa@gmail.com>
2018-12-16 04:14:35 +0900

    Add cytoscape

--- src/App.js
+++ src/App.js
@@ -1,36 +1,16 @@
-import * as  React from "react";
-import "./App.css";
-
-import logo from "./logo.svg"
+import React from 'react';
+import CytoscapeComponent from 'react-cytoscapejs';
 
 class App extends React.Component {
-  render() {
-    const logoOptions = {
-      alt: "logo",
-      className: "App-logo",
-      src: logo
-    };
-    const title = "こんにちはReact"
-    const targets = ["World", "Kanae", "Yukina"];
 
+  render(){
+    const elements = [
+       { data: { id: 1, label: 'Lean Analytics' }, position: { x: 100, y: 100 } },
+       { data: { id: 2, label: 'アントレプレナーの教科書' }, position: { x: 100, y: 200 } },
+       { data: { source: 1, target: 2, label: 'Edge from Node1 to Node2' } }
+    ];
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          {
-            // コメント
-          }
-          <img {...logoOptions} />
-          { }
-          { title && <h1 className="App-title">{title}</h1> }
-        </header>
-        {targets.map(target => (
-          <p className="App-intro">
-            Hello, {target}!
-          </p>
-        ))}
-      </div>
-    );
+    return <CytoscapeComponent elements={elements} style={ { width: '600px', height: '600px' } } />;
   }
 }
 
